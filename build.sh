aecce4af60211433c021f5781e38ec867c4b7c85
LysenkovIlya-Hexlet <lysenkov_iv21@mail.ru>
2025-07-12 19:31:31 +0300

    fix builder

--- build.sh
+++ build.sh
@@ -2,5 +2,12 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && psql -a -d "$DATABASE_URL" -f database.sql || echo "psql failed, but continuing"
 
+make install
+
+until pg_isready -d "$DATABASE_URL"; do
+  echo "Waiting for database..."
+  sleep 2
+done
+
+psql -a -d "$DATABASE_URL" -f database.sql
