b5527189bd54410497efa78737d10cb7ef05dc6e
LysenkovIlya-Hexlet <lysenkov_iv21@mail.ru>
2025-07-12 17:49:04 +0300

    Project Push

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/IlyaLysenkov-Hexlet/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/IlyaLysenkov-Hexlet/python-project-83/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=IlyaLysenkov-Hexlet_python-project-83&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=IlyaLysenkov-Hexlet_python-project-83)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=IlyaLysenkov-Hexlet_python-project-83&metric=coverage)](https://sonarcloud.io/summary/new_code?id=IlyaLysenkov-Hexlet_python-project-83)
+
+# Анализатор страниц
+
+Сайт проверки сайтов на SEO-пригодность
+Позволяет получить тему, заголовок и описание сайта
+
+Ссылка на Render проекта:
+
+## В проекте использован Python 3.10, Pip 24.3.1, uv 0.5.16
+
+Для разработки использовались следующие инструменты:
+
+Flask — фреймворк для создания веб-приложений на языке программирования Python
+
+Gunicorn — минивеб-сервер, осуществляющий запуск Python-приложения
+
+Requests — библиотека для языка Python, осуществляющая работу с HTTP-запросами
+
+Validators — модуль для проверки данных на соответствие критериям в Pythoт
+
+BeautifulSoup — эбиблиотека Python, используемая для парсинга HTML и XML документов
+
+ruff - линтер для uv
+
+## Clon project
+```python3
+git clone https://github.com/IlyaLysenkov-Hexlet/python-project-83.git
+cd python-project-83
+```
+Для хранения конфиденциональной информации создать файл .env в директории 
+page-analyzer 
+
+DATABASE_URL = postgresql://{username}{password}@{host}:{port}/{basename}
+
+SECRET_KEY = "{your_secret_key}"
+
+## Instll
+```python3
+make install
+```
+## Start localhost
+```python3
+make dev
+```
+
+## Start deploy
+```python3
+make render-start
+```
+
+
---
+++ page_analyzer/app.py
@@ -0,0 +1,105 @@
+import os
+
+import requests
+import validators
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+from page_analyzer.db_function import (
+    get_connection,
+    get_url,
+    get_url_check_result,
+    get_url_checks,
+    insert_check_result_with_id_url,
+    insert_url,
+    is_url_in_database,
+)
+from page_analyzer.functions import (
+    get_domain,
+    url_parser,
+)
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = get_connection(DATABASE_URL)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/urls')
+def get_sites():
+    sites = get_url(conn, 'all')
+    table = get_url_checks(conn)
+    return render_template(
+        'show.html',
+        sites=sites,
+        table=table,
+    )
+
+
+@app.route('/urls', methods=['POST'])
+def get_site():
+    url_for_check = request.form.to_dict().get('url')
+    url_length = len(url_for_check)
+    url = get_domain(url_for_check)
+    if validators.url(url) and url_length <= 255:
+        if is_url_in_database(conn, url):
+            flash('Страница уже существует', 'info')
+            id = get_url(conn, 'id', url)
+        else:
+            id = insert_url(conn, url)
+            flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('get_site_information', id=id))
+    else:
+        flash('Некорректный URL', 'danger')
+        return render_template('index.html',
+                           value=url_for_check), 422
+
+
+@app.route('/urls/<int:id>')
+def get_site_information(id):
+    data = get_url(conn, 'site', id)
+    if data:
+        check_result = get_url_check_result(conn, id)
+        return render_template(
+            'new.html',
+            data=data,
+            check_result=check_result,
+            id=id
+        )
+    return render_template('nopage.html'), 404
+
+
+@app.route('/urls/<int:id>/checks', methods=['POST'])
+def get_check_site(id):
+    url = get_url(conn, 'domain', id)
+    try:
+        response = requests.get(f'{url}')
+        response.raise_for_status()
+        status_code, h1, title, description = url_parser(response)
+        insert_check_result_with_id_url(conn, id, status_code,
+                                        h1, title, description)
+        flash('Страница успешно проверена', 'success')
+        return redirect(url_for('get_site_information', id=id, ))
+    except Exception:
+        flash('Произошла ошибка при проверке', 'danger')
+        return redirect(url_for('get_site_information', id=id, ))
+
+
+@app.errorhandler(404)
+def no_page(error):
+    return render_template('nopage.html'), 404
